4e97be7be07054f3b3c94232593ceeff1ca6fea1
raisabernardes <raisa.rmro@gmail.com>
2019-12-16 15:47:32 -0300

    7th Commit

--- app/src/main/java/com/faculdade/buddyride/Activities/AddFavoritesActivity.java
+++ app/src/main/java/com/faculdade/buddyride/Activities/AddFavoritesActivity.java
@@ -2,18 +2,24 @@ package com.faculdade.buddyride.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.faculdade.buddyride.Controllers.Facade;
+import com.faculdade.buddyride.Entities.FavoriteAddress;
 import com.faculdade.buddyride.R;
 
 public class AddFavoritesActivity extends AppCompatActivity {
 
     private ImageView mArrowBack;
-    //private TextView mAddress;
+    private ImageView mConfirmButton;
     private TextView mDescription;
+    private TextView mAddress;
+    private Facade facade;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,9 @@ public class AddFavoritesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_favorites);
 
         mArrowBack = findViewById(R.id.arrow_back);
+        mConfirmButton = findViewById(R.id.button_confirm);
+        mDescription = findViewById(R.id.description_field);
+        mAddress = findViewById(R.id.address_field);
 
         mArrowBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,5 +37,33 @@ public class AddFavoritesActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        mConfirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Catch input data
+
+                String catchDescription = null;
+                catchDescription = mDescription.getText().toString();
+                String catchAddress = mAddress.getText().toString();
+
+                if(catchAddress.isEmpty()){
+                    showToast("Invalid field. Try again.");
+
+                }else{
+                    //Save data
+                    FavoriteAddress favorite = new FavoriteAddress(catchDescription);
+                    facade.registerFavoriteAddress(favorite);
+                    startActivity(new Intent(AddFavoritesActivity.this, MainActivity.class));
+                }
+
+            }
+        });
+    }
+
+    //TOAST MESSAGE
+    private void showToast(String msg){
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
     }
 }
---
+++ app/src/main/java/com/faculdade/buddyride/Entities/LoggedUser.java
@@ -0,0 +1,9 @@
+package com.faculdade.buddyride.Entities;
+
+//Esta classe é apenas uma gambiarra para testes no qual um usuário deve estar logado.
+
+public class LoggedUser {
+
+    public static String id;
+
+}
